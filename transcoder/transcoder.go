69b7a39d0510aacb0fb740182a412a3f06766afb
Jonathan Llovet <jonathan.llovet@gmail.com>
2021-06-27 12:19:52 -0400

    Fill out stubs for uni to beta in transcoder, helper functions

--- transcoder/transcoder.go
+++ transcoder/transcoder.go
@@ -1,15 +1,84 @@
 package transcoder
 
 import (
-	"golang.org/x/text/unicode/norm"
+	"bufio"
+	"io"
+	"log"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+	// "github.com/golang-collections/collections/trie"
+	// "golang.org/x/text/unicode/norm"
 )
 
+// UniToBeta converts a unicode string to a betacode string
 func UniToBeta(uni string) (beta string) {
-	beta = string(norm.NFC.String(UNICODE_MAP[uni]))
-	return beta
+	u := UNICODE_MAP
+
+	transform := []string{}
+	b := bufio.NewReader(strings.NewReader(uni))
+	for {
+		if r, _, err := b.ReadRune(); err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				log.Fatal(err)
+			}
+		} else {
+			// check whether betacode value in map
+			if betaValue, ok := u[string(r)]; ok {
+				// found in unicode map, replace with betacode value
+				transform = append(transform, betaValue)
+			} else {
+				// not found in unicode map, keep value
+				transform = append(transform, string(r))
+			}
+		}
+	}
+	converted := strings.Join(transform, "")
+	return converted
+
+	// beta = string(norm.NFC.String(UNICODE_MAP[uni]))
+	// return beta
 }
 
+// BetaToUni converts a betacode string to a unicode string
 func BetaToUni(beta string) (uni string) {
 	uni = string(BETACODE_MAP[beta])
 	return uni
+}
+
+// findLongestBetaTokenLen returns the maximum length of a single betacode token
+func findLongestBetaTokenLen(BETACODE_MAP map[string]string) (maxBetaTokenLen int) {
+	maxBetaTokenLen = -1
+	for beta := range BETACODE_MAP {
+		if utf8.RuneCountInString(beta) > maxBetaTokenLen {
+			maxBetaTokenLen = utf8.RuneCountInString(beta)
+		}
+	}
+	return
+}
+
+var maxBetaTokenLen int = findLongestBetaTokenLen(BETACODE_MAP)
+
+// Special characters that need their own references to rewrite with
+var finalLowerCaseSigma string = `ς`  // `s2`, \u03c2
+var medialLowerCaseSigma string = `σ` // `s`,  \u03c3
+var betaApostrophe string = `’`       // `\'` \u2019
+var betaPunctuation []string = []string{
+	`·`, // `:`,  \u00b7
+	`’`, // `\'`, \u2019
+	`‐`, // `-`,  \u2010
+	`—`, // `_`,  \u2014
+}
+
+// penultimateSigmaWordFinal returns whether the penultimate character is a sigma
+// and is in a context where it should be treated as a medial sigma
+func penultimateSigmaWordFinal(text string) (p bool) {
+	l := utf8.RuneCountInString(text) // length in runes
+	p = utf8.RuneCountInString(text) > 1 &&
+		string(text[l-2]) == medialLowerCaseSigma &&
+		!unicode.IsLetter(rune(text[l-1])) &&
+		string(text[l-1]) != betaApostrophe
+	return p
 }
